8445985ab76f9872f55a1d64ef5c81607008497c
vadim <cojocaru.vadim@gmail.com>
2015-03-31 15:05:08 +0300

    rooms management

--- resources/views/admin/rooms/index.blade.php
+++ resources/views/admin/rooms/index.blade.php
@@ -6,7 +6,7 @@
     <section class="wrapper" ng-app="roomApp" ng-controller="roomController">
             <h1>Rooms index view</h1>
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <input type='text' ng-model="room.title">
                     <button class="btn btn-primary btn-md"  ng-click="addRoom()">Add</button>
                     <i ng-show="loading" class="fa fa-spinner fa-spin"></i>
@@ -14,13 +14,30 @@
             </div>
             <hr>
             <div class="row">
-                <div class="col-md-4">
-                    <table class="table table-striped">
-                        <tr ng-repeat='room in rooms'>
-                            <td><input type="checkbox" ng-true-value="1" ng-false-value="'0'" ng-model="room.reserved" ng-change="updateRoom(room)"></td>
-                            <td><% room.title %></td>
-                            <td><button class="btn btn-danger btn-xs" ng-click="deleteRoom($index)">  Delete</button></td>
+                <div class="col-md-12">
+                    <table class="table table-striped table-advance table-hover">
+                        <thead>
+                        <tr>
+                            <th><i class="fa fa-bullhorn"></i> Room title</th>
+                            <th class="hidden-phone"><i class="fa fa-question-circle"></i> Descrition</th>
+                            <th><i class="fa fa-bookmark"></i> Room type</th>
+                            <th><i class=" fa fa-edit"></i> Reserved</th>
+                            <th></th>
                         </tr>
+                        </thead>
+                        <tbody>
+                        <tr ng-repeat='room in rooms'>
+                            <td><a href="#"><% room.title %></a></td>
+                            <td class="hidden-phone">Lorem Ipsum dorolo imit</td>
+                            <td> - </td>
+                            <td><input type="checkbox" ng-true-value="1" ng-false-value="'0'" ng-model="room.reserved" ng-change="updateRoom(room)"></td>
+                            <td>
+                                <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                                <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-danger btn-xs" ng-click="deleteRoom($index)"><i class="fa fa-trash-o "></i></button>
+                            </td>
+                        </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
