2102a9bc14d3103db290e7600c1d54e46b9b02d7
vadim <cojocaru.vadim@gmail.com>
2015-04-06 17:41:25 +0300

    some ui and usability improvements

--- resources/views/admin/rooms/index.blade.php
+++ resources/views/admin/rooms/index.blade.php
@@ -10,34 +10,35 @@
     <section class="wrapper" ng-app="roomApp" ng-controller="roomController">
         <h1>Rooms</h1>
         <alert ng-repeat="alert in alerts" type="<%alert.type%>" close="closeAlert($index)"><%alert.msg%></alert>
-        <section class="panel">
-            <header class="panel-heading">Add new room</header>
-            <div class="panel-body">
-                <form class="form-inline" role="form">
-                    <div class="form-group">
-                        <div class="btn-group" dropdown is-open="status.isopen">
-                            <button type="button" class="btn btn-primary dropdown-toggle" dropdown-toggle ng-disabled="disabled">
-                                <% roomTypeSelected.title %> <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu" role="menu">
-                                <li ng-repeat="roomType in roomTypes"><a ng-click="setRoomTypeSelected(roomType)"><% roomType.title %></a></li>
-                            </ul>
-                        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <section class="panel">
+                    <header class="panel-heading">Add new room</header>
+                    <div class="panel-body">
+                        <form role="form">
+                            <div class="form-group">
+                                <div class="btn-group" dropdown is-open="status.isopen">
+                                    <button type="button" class="btn btn-primary dropdown-toggle" dropdown-toggle ng-disabled="disabled">
+                                        <% roomTypeSelected.title %> <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li ng-repeat="roomType in roomTypes"><a ng-click="setRoomTypeSelected(roomType)"><% roomType.title %></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" ng-model="room.title" class="form-control" id="exampleInputEmail2" placeholder="Enter title">
+                            </div>
+                            <div class="form-group">
+                                <textarea ng-model="room.description" rows="3" class="form-control" placeholder="Enter description" id="description"></textarea>
+                            </div>
+                            <button class="btn btn-success" ng-click="addRoom()">Add</button>
+                            <i ng-show="loading" class="fa fa-spinner fa-spin"></i>
+                        </form>
                     </div>
-                    <div class="form-group">
-                        <label class="sr-only" for="exampleInputEmail2">Title</label>
-                        <input type="text" ng-model="room.title" class="form-control" id="exampleInputEmail2" placeholder="Enter title">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" ng-model="room.description" class="form-control" placeholder="Enter description">
-                    </div>
-                    <button class="btn btn-success" ng-click="addRoom()">Add</button>
-                    <i ng-show="loading" class="fa fa-spinner fa-spin"></i>
-                </form>
+                </section>
             </div>
-        </section>
-        <hr>
-
+        </div>
         <div class="row">
             <div class="col-md-12">
                 <div class="text-center">
@@ -51,15 +52,16 @@
                 <div class="panel">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-md-9">
+                            <div class="col-md-8">
                                 <h3><% room.title %> <span class="badge bg-success"><% indexedRoomTypes[room.room_types_id] %></span></h3>
                                 <p><% room.description %></p>
                                 <span class="label label-primary">Reserved</span> : <input type="checkbox" ng-true-value="1" ng-false-value="'0'" ng-model="room.reserved" ng-change="updateReservedRoom(room)">
                             </div>
-                            <div class="col-md-3">
+                            <div class="col-md-4">
                                 <div class="pull-right">
-                                    <button ng-click="setEditedRoom(room);edit(room);" class="btn btn-primary"><i class="fa fa-pencil"></i> Edit</button>
-                                    <button class="btn btn-danger" ng-click="confirmDelete($index)"><i class="fa fa-trash-o "></i> Delete</button>
+                                    <button class="btn btn-primary btn-sm" ng-click="edit(room);"><i class="fa fa-pencil"></i> Edit</button>
+                                    <button class="btn btn-info btn-sm" ng-click=""><i class="fa fa-picture-o "></i> Add images</button>
+                                    <button class="btn btn-danger btn-sm" ng-click="confirmDelete($index)"><i class="fa fa-trash-o "></i> Delete</button>
                                 </div>
                             </div>
                         </div>
@@ -101,7 +103,7 @@
                         </div>
                         <div class="form-group">
                             <label for="description">Description</label>
-                            <input type="text" ng-model="editedRoom.description" class="form-control" id="description">
+                            <textarea rows="5" ng-model="editedRoom.description" class="form-control" id="description"></textarea>
                         </div>
                     </form>
                 </div>
