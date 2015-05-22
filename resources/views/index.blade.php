204f9dfb437f45252085a27247599f6103bbc70e
vadim <cojocaru.vadim@gmail.com>
2015-05-22 12:48:40 +0300

    calendar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,50 @@
 
     <script type="text/javascript">
         $(function() {
-            $('input[name="daterange"]').daterangepicker();
+
+            $('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+
+            $('#reportrange').daterangepicker({
+                format: 'MM/DD/YYYY',
+                startDate: moment().subtract(29, 'days'),
+                endDate: moment(),
+                minDate: '01/01/2012',
+                maxDate: '12/31/2015',
+                dateLimit: { days: 60 },
+                showDropdowns: true,
+                showWeekNumbers: true,
+                timePicker: false,
+                timePickerIncrement: 1,
+                timePicker12Hour: true,
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                },
+                opens: 'left',
+                drops: 'down',
+                buttonClasses: ['btn', 'btn-sm'],
+                applyClass: 'btn-primary',
+                cancelClass: 'btn-default',
+                separator: ' to ',
+                locale: {
+                    applyLabel: 'Submit',
+                    cancelLabel: 'Cancel',
+                    fromLabel: 'From',
+                    toLabel: 'To',
+                    customRangeLabel: 'Custom',
+                    daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+                    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                    firstDay: 1
+                }
+            }, function(start, end, label) {
+                console.log(start.toISOString(), end.toISOString(), label);
+                $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+            });
+
         });
     </script>
 @stop
@@ -79,10 +122,14 @@
                         <form name="registrationForm" ng-app="registrationApp" ng-controller="registrationCtrl" novalidate method="post">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <input type="text" name="daterange" value="05/23/2015 - 05/31/2015" />
+                                    <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                                        <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                                        <span></span> <b class="caret"></b>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="row">
+                            <br><br>
+                            {{--<div class="row">
                                 <div class="col-xs-6 date_of_coming">
                                     <p>Date de debut</p>
                                     <input type="date" name="in">
@@ -91,7 +138,7 @@
                                     <p>Date de fin</p>
                                     <input type="date" name="out">
                                 </div>
-                            </div>
+                            </div>--}}
                             <div class="row">
                                 <div class="col-xs-6 num_of_adults">
                                     <p>Adultes</p>
