a286187e10d9656bbb135ce505f742407a8e41b9
vadim <cojocaru.vadim@gmail.com>
2015-05-25 17:25:08 +0300

    date ranges

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,14 +31,14 @@
                 timePicker: false,
                 timePickerIncrement: 1,
                 timePicker12Hour: true,
-                ranges: {
+                /*ranges: {
                     'Today': [moment(), moment()],
                     'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
                     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
                     'This Month': [moment().startOf('month'), moment().endOf('month')],
                     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-                },
+                },*/
                 opens: 'left',
                 drops: 'down',
                 buttonClasses: ['btn', 'btn-sm'],
