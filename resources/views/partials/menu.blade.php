8e05c223feffb5073aace9749dac3e3b0f04339b
vadim <cojocaru.vadim@gmail.com>
2016-04-27 15:27:21 +0300

    fixed ghix

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -6,7 +6,7 @@
             <li class="@if(Request::is('hotel')) active @endif"><div class="header_separator"></div> <a href="{{url('hotel')}}">Hôtel</a></li>
             <li class="@if(Request::is('room')) active @endif" id="nav_rooms"><div class="header_separator"></div> <a href="{{url('room')}}">Chambres / Appartement</a></li>
             <li class="@if(Request::is('restaurant')) active @endif"><div class="header_separator"></div> <a href="{{url('restaurant')}}">Restaurant / Bar</a></li>
-            <li class="@if(Request::is('offers')) active @endif"><div class="header_separator"></div> <a href="https://sb.ghix.net/default.aspx?view=SuperBookingPackages&action=List&HotelID=2986959&Language=4">Offres / Packages</a></li>
+            <li class="@if(Request::is('offers')) active @endif"><div class="header_separator"></div> <a target="_blank" href="https://sb.ghix.net/default.aspx?view=SuperBookingPackages&action=List&HotelID=2986959&Language=4">Offres / Packages</a></li>
             {{--<li><img src="/img/header_and_footer_links_seperator.png"> <a href="{{url('wellness_center')}}">Centre bien-etre / Loisirs</a></li>--}}
             <li class="@if(Request::is('contact')) active @endif"><div class="header_separator"></div> <a href="{{url('contact')}}">Contact</a></li>
             {{--<li>--}}
